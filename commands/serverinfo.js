60d99fac8bb3fe7403c0465bb530688679314706
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-16 23:48:57 +0530

    Create serverinfo.js
    Serverinfo command

---
+++ commands/serverinfo.js
@@ -0,0 +1,33 @@
+module.exports = ({
+    name: "serverinfo",
+    description: "Shows the info of the current server",
+    aliases: "si",
+    code: `$title[Info of $serverName]
+$replaceText[$replaceText[$checkCondition[$serverIcon!=null];true;$thumbnail[$serverIcon]];false;]
+$footer[Requested by $userTag[$authorID];$authorAvatar]
+$addTimestamp
+$color[RANDOM]
+$addField[**Roles**[$roleCount];**Highest Role:** <@&$highestServerRole>
+**Lowest Role:** <@&$lowestServerRole>;yes]
+$addField[**Prefixes**;$getServerVar[prefix];yes]
+$addField[**Members**[$membersCount];**Humans:** $math[$membersCount-$botCount]
+**Bots:** $botCount;yes]
+$addField[**Emoji**[$emojiCount[all]];**Normal:** $emojiCount[normal]
+**Animated:** $emojiCount[animated];yes]
+$addField[**Info**;**Name:** $serverName
+**Server ID:** $guildID
+**Media Content Filter:** ✅$serverContentFilter
+**Verification Level:** ✅$serverVerificationLevel
+**Region:** $toLocaleUppercase[$serverRegion]
+**Voice:** $serverPreferredLocale
+**Boost Count:** $serverBoostCount
+**Boost Tier:** $serverBoostLevel;yes]
+$addField[**Channels**[$channelCount];
+**Category:** $channelCount[category]
+**Text:** $channelCount[text]
+**Voice:** $channelCount[voice];yes]
+$addField[**Features**;$replaceText[$replaceText[$checkCondition[$serverFeatures!=];true;✅$serverFeatures[$guildID;
+✅]];false;🚫NaN];yes]
+$addField[**Owner**;$userTag[$ownerID];yes]
+$suppressErrors[An Error Occurred!!]`
+});
