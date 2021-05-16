0dcfe76b3553d81a4c3dbbdf8dd7a279d1ff3163
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-16 20:47:30 +0530

    Create reload.js
    Reload cmd to refresh all the commands.

---
+++ commands/reload.js
@@ -0,0 +1,6 @@
+module.exports = ({
+    name: "refresh",
+    aliases: "reload",
+    description: "reloads all the commands!!",
+    code: `$updateCommands Successfully updated all the commands!`
+});
