46b9d5c5cf60ad86edaddc4ead8a94cbad5a3cbf
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-16 23:13:10 +0530

    Update stats.js
    Some more changes

--- commands/stats.js
+++ commands/stats.js
@@ -2,6 +2,17 @@ module.exports = ({
   name: "botstats",
   aliases: ["bot","stats"],
   code: `$title[My Stats]
+$addField[> __Package Info__:;**- aoi.js:** **\`v$packageVersion\`**
+**- dbdjs.db:** **\`$djsEval[require('dbdjs.db').version;yes]\`**
+**- Node.js:** **\`$djsEval[process.version;yes]\`**
+**- @$username[$clientID]:** **\`v1.0.0\`**;yes]
+$addField[> __CPU INFO__:;**- CPU Usage:** **\`$cpu%\/100%\`**
+**- Ram Usage:** **\`$ramMB\/1024MB\`**
+**- CPU Model:** **\`$djsEval[require('os').cpus()[0].model;yes]\`**
+**- CPU Speed:** **\`$djsEval[require('os').cpus()[0].speed;yes] MHZ\`**
+**- Architecture:** **\`$djsEval[process.arch;yes] BIT\`**
+**- Core:** **\`$djsEval[require('os').cpus().length;yes] Core\`**
+**- OS:** **\`$djsEval[require('os').platform();yes]\`**;yes]
 $addField[> __Normal Stats__:;**• Name:** **\`$username[$clientID]\`**
 **• ID:** **\`$clientID\`**
 **• Owner:** **\`$userTag[746113176885657701], $userTag[682592010661527565], $userTag[716761186812821604] and $userTag[819586427968552971]\`**
@@ -12,17 +23,6 @@ $addField[> __Normal Stats__:;**• Name:** **\`$username[$clientID]\`**
 **• Joined At:** **\`$memberJoinedDate[$clientID]\`**
 **• Uptime:** **\`$uptime\`**
 **• Commands:** **\`$commandsCount\`**;yes]
-$addField[> __CPU INFO__:;**- CPU Usage:** **\`$cpu%\/100%\`**
-**- Ram Usage:** **\`$ramMB\/1024MB\`**
-**- CPU Model:** **\`$djsEval[require('os').cpus()[0].model;yes]\`**
-**- CPU Speed:** **\`$djsEval[require('os').cpus()[0].speed;yes] MHZ\`**
-**- Architecture:** **\`$djsEval[process.arch;yes] BIT\`**
-**- Core:** **\`$djsEval[require('os').cpus().length;yes] Core\`**
-**- OS:** **\`$djsEval[require('os').platform();yes]\`**;yes]
-$addField[> __Package Info__:;**- aoi.js:** **\`v$packageVersion\`**
-**- dbdjs.db:** **\`$djsEval[require('dbdjs.db').version;yes]\`**
-**- Node.js:** **\`$djsEval[process.version;yes]\`**
-**- @$username[$clientID]:** **\`v1.0.0\`**;yes]
 
 $footer[Requested By $username;$userAvatar[$authorID]]
 $addTimestamp
