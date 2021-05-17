6f974796ffe8d80b9f3253f5a659774327835ef7
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-17 14:00:09 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,7 @@
 module.exports = ({
 name: "ping",
+usage: "\`+ping\`",
+description: "Shows the Bot Latency and Websocket Latency.",
 code: `Pinging...
 $editIn[1s;🏓**Pong!**
 **Bot Latency:** $botPingms
