c3687ecd31e028ab9116195556d72e0f40bd627a
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-18 18:16:39 +0530

    Updated new cmds

--- commands/Help.js
+++ commands/Help.js
@@ -7,7 +7,7 @@ $footer[$userTag[$authorID];$authorAvatar]
 $description[> **Prefix for $serverName:** **\`\"$get[p]\"\`**]
 $addField[$replaceText[$replaceText[$checkCondition[$message==];true;$get[helpn]];false;$get[cmdn]];$replaceText[$replaceText[$checkCondition[$message==];true;**\`\`\`$get[help]\`\`\`**];false;$get[cmd]];yes]
 $let[helpn;> **All Commands**[$commandsCount]]
-$let[help;$get[p]ping, $get[p]setmuterole, $get[p]mute, $get[p]unmute, $get[p]tempmute, $get[p]resetmute, $get[p]userinfo, $get[p]serverinfo, $get[p]refresh, $get[p]stats, $get[p]prefix, $get[p]eval, $get[p]exec, $get[p]reboot.
+$let[help;$get[p]ping, $get[p]setmuterole, $get[p]mute, $get[p]unmute, $get[p]tempmute, $get[p]resetmute, $get[p]userinfo, $get[p]serverinfo, $get[p]refresh, $get[p]stats, $get[p]prefix, $get[p]eval, $get[p]exec, $get[p]reboot, $get[p]aoi.
 For seeing more info of a specific command type $get[p]help (command name).]
 $let[cmdn;$replaceText[$replaceText[$checkCondition[$commandInfo[$message;name]!=];true;$get[cmn]];false;ERROR]
 $let[cmd;$replaceText[$replaceText[$checkCondition[$commandInfo[$message;name]!=];true;$get[cm]];false;**No commands found with that name!!**]
