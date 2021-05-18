af29c64234844432ddcf826f163efe140b2f55ac
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-18 13:05:17 +0530

    Create eval.js
    Created eval to test some new codes.

---
+++ commands/eval.js
@@ -0,0 +1,13 @@
+module.exports = ({
+name: "eval",
+usage: "+eval <code>",
+description: "Evaluates the discord.js codes. Only the developers of the bot can use it!!",
+code: `$addField[OUTPUT;\`\`\`kt
+$djsEval[$message;yes]\`\`\`;yes]
+$addField[INPUT;\`\`\`kt
+$message\`\`\`;yes]
+$color[RANDOM]
+$onlyIf[$djsEval[$message;yes]!=undefined;]
+$suppressErrors[{title:ERROR}{description:{error}}{color:RED}]
+$onlyForIDs[819586427968552971;682592010661527565;742945200082780160;746113176885657701;716761186812821604;**⛔ You are not my developer to perform this action!!**]`
+});
