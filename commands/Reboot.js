741202b3ae4e010f5de1ef8187161ea6640fcd53
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-28 14:25:05 +0530

    Update Reboot.js

--- commands/Reboot.js
+++ commands/Reboot.js
@@ -3,6 +3,7 @@ name: "reboot",
 aliases: "restart",
 usage: "\`+reboot\`",
 description: "Reboots the bot. Its limited to only developers of the bot!!",
+category: "Developer",
 code: `$reboot[Index.js]
 $sendMessage[{field:**⚠️ Warning ⚠️**:**\`\`\`
 Manual Reboot has started by $userTag[$authorID]!! Now i will stop to respond to all commands for 1-2 min!\`\`\`**:yes}{color:RANDOM}{timestamp:ms}{thumbnail:$userAvatar[$clientID]};no]
