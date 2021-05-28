77e8276ccb59e3fef82770eb3069aeca2fe2926c
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-28 14:28:42 +0530

    Update setmute.js

--- commands/setmute.js
+++ commands/setmute.js
@@ -2,6 +2,7 @@ module.exports = ({
     name: "setmuterole",
     usage: "\`+setmuterole <role>\`",
     description: "Sets the specified role as the muterole of the server.",
+    category: "Moderation",
     code: `$setServerVar[mute;$findRole[$message]]
 $title[Mute Role Set!!]
 $description[$username set <@&$findRole[$message]> as the mute role!!]
@@ -10,9 +11,7 @@ $color[RANDOM]
 $addTimestamp
 $onlyIf[$rolePosition[$highestRole[$clientID]]<$rolePosition[$findRole[$message]];**⛔ That role is higher/equal than me on role position!!**]
 $onlyIf[$roleExists[$findRole[$message]]==true;Please specify a valid role!!]
-
 $onlyIf[$getServerVar[mute]==;**⛔ Your mute role is already set to <@&$getServerVar[mute]>.Please reset that for setting a new mute role!!**]
 $onlyIf[$message!=;**⛔ Please specify a role for setting it as mute role!!**]
-
 $onlyPerms[manageserver;You dont have \`MANAGE SERVER\` perms to perform this action!!]`
     });
