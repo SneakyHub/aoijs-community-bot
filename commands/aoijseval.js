9d412f3e122d2605dbbc0654391e71f0cf2a8b30
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-28 14:17:40 +0530

    Did some changes

--- commands/aoijseval.js
+++ commands/aoijseval.js
@@ -2,10 +2,7 @@ module.exports = ({
     name: "aoi",
     usage: "+aoi <code>",
     description: "Evaluates aoijs codes. Its limited to only Developers!!",
-    code: `$title[Evaluated Command]
-$addField[OUTPUT:;\`\`\`kt
-$eval[$message]\`\`\`;yes]
-$addField[INPUT:;\`\`\`kt
-$message\`\`\`;yes]
+    category: "Developer",
+    code: `$eval[$message]
 $onlyForIDs[819586427968552971;682592010661527565;742945200082780160;746113176885657701;716761186812821604;**⛔ You are not my developer to perform this action!!**]`
 });
