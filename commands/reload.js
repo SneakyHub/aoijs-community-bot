c968f06d1b4f57a223e0ed0292f4b2a8a294153d
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-28 14:26:05 +0530

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -3,5 +3,8 @@ module.exports = ({
     aliases: "reload",
     usage: "\`+refresh\`",
     description: "Reloads all the commands!!",
-    code: `$updateCommands Successfully updated all the commands!`
+    category: "Developer",
+    code: `$updateCommands Successfully updated all the commands!
+$onlyForIDs[$joinTextSplit[;];742945200082780160;**⛔ Only developers can perform this action!!**]
+$textSplit[$botOwnerID;,]`
 });
