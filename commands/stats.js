855be469f3f901ce2a32d9235e907d73383e01e9
BeastBoyADI <82759519+BeastBoyADI@users.noreply.github.com>
2021-05-28 14:29:34 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -3,13 +3,14 @@ module.exports = ({
   aliases: ["bot","stats"],
   usage: "\`+botstats\`",
   description: "Shows all info of the bot.",
+  category: "BotInfo",
   code: `$title[My Stats]
 $addField[> __Package Info__:;**- aoi.js:** **\`v$packageVersion\`**
 **- dbdjs.db:** **\`$djsEval[require('dbdjs.db').version;yes]\`**
 **- Node.js:** **\`$djsEval[process.version;yes]\`**
 **- @$username[$clientID]:** **\`v1.0.0\`**;yes]
 $addField[> __CPU INFO__:;**- CPU Usage:** **\`$cpu%\/100%\`**
-**- Ram Usage:** **\`$ramMB\/1024MB\`**
+**- Ram Usage:** **\`$ramMB\/512MB\`**
 **- CPU Model:** **\`$djsEval[require('os').cpus()[0].model;yes]\`**
 **- CPU Speed:** **\`$djsEval[require('os').cpus()[0].speed;yes] MHZ\`**
 **- Architecture:** **\`$djsEval[process.arch;yes] BIT\`**
@@ -25,7 +26,6 @@ $addField[> __Normal Stats__:;**• Name:** **\`$username[$clientID]\`**
 **• Joined At:** **\`$memberJoinedDate[$clientID]\`**
 **• Uptime:** **\`$uptime\`**
 **• Commands:** **\`$commandsCount\`**;yes]
-
 $footer[Requested By $username;$userAvatar[$authorID]]
 $addTimestamp
 $thumbnail[$userAvatar[$clientID]]
