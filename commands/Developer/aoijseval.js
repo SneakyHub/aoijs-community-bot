96c6d908eea8a70110cffaa1d667c8039c3eb81f
Southy <84360747+southern-ttv@users.noreply.github.com>
2022-08-06 23:59:02 -0400

    Delete commands directory

--- commands/Developer/aoijseval.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-    name: "aoi",
-    usage: "+aoi <code>",
-    description: "Evaluates aoijs codes. Its limited to only Developers!!",
-    category: "Developer",
-    code: `$eval[$message]
-$onlyForIDs[819586427968552971;682592010661527565;742945200082780160;746113176885657701;716761186812821604;**⛔ You are not my developer to perform this action!!**]`
-});
